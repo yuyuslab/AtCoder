1b750abe7503f4214a94b31b8b0594872927a9fd
yuyuslab <yuyuslab@gmail.com>
2024-02-24 20:51:58 +0900

    Feb_17_2024

---
+++ Feb_17/contest_Feb_17.py
@@ -0,0 +1,60 @@
+# N = int(input())
+# i = 0
+# string = ""
+# while i < N:
+#     string += "10"
+#     i += 1
+# output = string + "1"
+
+# print(output)
+
+
+# N = int(input())
+
+# A = list(map(int, input().split()))
+
+# c = 1
+# cl = []
+# while c < N:
+#     ST = list(map(int, input().split()))
+#     cl += [ST]
+#     c += 1
+
+# pml = []
+
+# import random
+
+# chol = [2,1,2,3]
+
+# # i = random.randrange(N)
+# # while A[i] >= cl[i][0]:
+# #     A[i] - cl[i][0]
+# #     A[i+1] + cl[i][1]
+# #     pml += [A[N-1]]
+# #     i = random.randrange(N)
+# def modA (A):
+#     A[i-1] - cl[i-1][0]
+#     A[i] + cl[i-1][1]
+#     return A
+
+# for i in chol:
+#     A = modA(A)
+#     pml += [A[N-1]]
+
+# print(pml)
+
+
+
+K = [1,2,3]
+K = K[1] - 2
+print(K)
+
+
+
+
+
+
+
+
+
+
