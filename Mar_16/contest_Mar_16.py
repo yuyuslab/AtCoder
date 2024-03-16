8a40691c2219d38d299e3d4d1c85ca8ac4e556f6
yuyuslab <yuyuslab@gmail.com>
2024-03-16 23:22:02 +0900

    March 16th 2024

---
+++ Mar_16/contest_Mar_16.py
@@ -0,0 +1,28 @@
+# S =str(input())
+# sl = S[1:-1]
+# comp = ""
+# for i in range(len(sl)):
+#     comp += "="
+
+# if S[0] == "<":
+#     if S[-1] == ">":
+#         if sl == comp:
+#             print("Yes")
+#         else: print("No")
+#     else: print("No")
+# else: print("No")
+
+# X = int(input())
+
+# Min = X//10
+
+# if Min == X/10: 
+#     print(Min)
+# else:
+#     print(Min + 1)
+
+# X = int(input())
+# print((X + 9) // 10)
+
+# あーーーAyuさん頭いいの〜
+print(-(-int(input())//10))
