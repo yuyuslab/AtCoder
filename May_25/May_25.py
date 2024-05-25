b55aa9968af1882ec28326c9009f3889fcfd5193
yuyuslab <yuyuslab@gmail.com>
2024-05-25 23:31:25 +0900

    May 25

---
+++ May_25/May_25.py
@@ -0,0 +1,25 @@
+# A, B = map(int, input().split())
+
+# if A == B:
+#     print(-1)
+# elif A + B == 3:
+#     print(3)
+# elif A + B == 5:
+#     print(1)
+# elif A + B == 4:
+#     print(2)
+
+
+# N, M = map(int, input().split())
+# A = list(map(int, input().split()))
+# B = list(map(int, input().split()))
+
+# C = A + B
+# C.sort()
+
+
+# for i in range(len(C) - 1):
+#     if C[i] in A and C[i + 1] in A:
+#         print("Yes")
+#         exit()
+# print("No")
