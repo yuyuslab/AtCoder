800f96b1de2c55d0a687c4705cb8ed3c2f821647
yuyuslab <yuyuslab@gmail.com>
2024-09-26 16:04:24 +0900

    Long time no code for AtCoder Sep 21

---
+++ Sep_21/Sep_21.py
@@ -0,0 +1,19 @@
+# S = map(str, input())
+
+# lst = []
+
+# for i in S:
+#     if i != ".":
+#         lst.append(i)
+#     else:
+#         lst
+
+# print("".join(lst))
+
+M = int(input())
+
+if M%3 == 0:
+    i = M//3
+    print(i)
+else:
+    None
